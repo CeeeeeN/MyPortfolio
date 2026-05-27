c03efe53e1aab3dd8ea876228ad686534bb87eff
CeeeeeN <chaelsybenj@gmail.com>
2026-05-27 14:42:05 +0800

    additions

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="py-20 px-[10%] border-t border-border flex justify-between font-display text-lg text-muted">
+            <div>© 2026 // CHAELSY_BENJ_PORTFOLIO</div>
+        </footer>
+    )
+}
